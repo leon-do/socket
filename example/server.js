72b42e73df1015f1b0214252f918d0bfa481327c
Leon Do <leondo123@gmail.com>
2021-04-21 01:46:06 -0400

    Update server.js

--- example/server.js
+++ example/server.js
@@ -1,6 +1,10 @@
 const app = require('express')()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on('connection', socket => {
   socket.on('invoice', data => {
